0a74a3621a1961af13ad2d7faf1523aacf4b6834
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2023-11-26 23:56:40 -0600

    Refactor Stage 1 - Added functions to components
    The deletion, update, restoration and editor panel functions are rebuilt in every component class instead of the component manager. Should make deletion, update, restoration easier. And also makes adding and handling components easier, but with more component class code. Also, more perhaps we can make a generic component class from which all functions inherit a majority of the new functions.

--- GUI_designer_DPG/components/panel.py
+++ GUI_designer_DPG/components/panel.py
@@ -19,6 +19,13 @@ class Panel:
         self.parent = None
         self.children = []
 
+        # Objects
+        self.panels = None
+        self.components = None
+        self.component_manager = None
+        self.parent_panel = None
+        self.editor_panel = None
+
         # Parameters
         self.position = pos
         self.width = width
@@ -38,4 +45,42 @@ class Panel:
 
     def add(self):
         dpg.add_window(label=self.label, tag=self.tag, width=self.width, height=self.height, pos=self.position, on_close=self.on_close, user_data=self.tag)
+
+    def restore(self):
+        self.add()
+        for child in self.children:
+            self.panels.get(child).restore()
+
+    def update_panel_data(self):
+        self.position = dpg.get_item_pos(self.tag)
+        self.width = dpg.get_item_width(self.tag)
+        self.height = dpg.get_item_height(self.tag)
+
+    def update(self):
+        self.update_panel_data()
+        dpg.delete_item(self.parent_panel)
+        self.restore(self.parent_panel)
+
+    def delete(self):
+        # Get All Children
+        for child in self.children:
+            # Recursive Deletion of Children
+            self.components.get(child).delete()
+        # Deletion of Panel from Panel Dict (after deleting all children)
+        self.panels.pop(self.tag)
+        for panel_tag, panel in self.panels.items():
+            self.set_active_panel_callback(panel_tag, 0)
+            return
     
+    def editor_callback(self, sender, value, parameter_name):
+        setattr(self, parameter_name, value)
+
+    def editor_controls(self):
+        dpg.delete_item(item=self.editor_panel, children_only=True)
+        # Get and add all Component Parameters
+
+        # TODO : Add all parameters from component manager
+
+        # Add Update and Delete Buttons
+        dpg.add_button(label="UPDATE", parent=self.editor_panel, callback=self.update, user_data=self.tag)
+        dpg.add_button(label="DELETE", parent=self.editor_panel, callback=self.delete, user_data=self.tag)
