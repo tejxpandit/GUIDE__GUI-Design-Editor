206fe7768cb632dfa0da84de8cdcc98c8a75a722
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2023-12-21 23:18:11 -0600

    Added Window and Component Dictionaries
    Added separate dictionaries for windows and components. Also, added methods for adding and removing components from the census. The census dictionary marks the number of components used so far by the builder. It can help with default numbering. It does not represent the current count, as some components might have been deleted. But it represents the total number of components of a type that have ever been instantiated.

--- GUI_designer_DPG/sample_gui_format.py
+++ GUI_designer_DPG/sample_gui_format.py
@@ -3,8 +3,11 @@
 # Author : Tej Pandit
 # Date : Dec, 2023
 
-# Make GUI Project Dictionary
-project = dict()
+from components import panel
+from components import component
+
+# CENSUS
+# Make GUI Project Census Dictionary
 census = dict()
 
 # Component Numbering
@@ -12,8 +15,30 @@ census.update({"window" : 0})
 census.update({"button" : 0})
 census.update({"slider" : 0})
 
-# Add to Windows to Project
-name = "window " + census.get("window")
-project.update({"Window 1" : "apple"})
+# Add or Remove Component from Census
+census["window"] = census.get("window") + 1
+census["window"] = census.get("window") - 1
+
+#############################################
+
+# WINDOWS
+# Make GUI Project Window Dictionary
+windows = dict()
+
+# Add a Window to the Project
+default_window_name = panel.type + "_" + census.get(panel.type)
+windows.update({default_window_name : []})
+
+#############################################
+
+# COMPONENTS
+# Make GUI Project Component Dictionary
+components = dict()
+
+# Add a Component to a Window
+default_component_name = component.type + "_" + census.get(component.type)
+components.update({default_component_name : []})
+
+#############################################
 
 # TODO : Finish template
