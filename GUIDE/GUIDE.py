ba654941399bb9c8037e23b4fc6eea6a527dd281
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-04-02 20:28:09 -0500

    GUIDE launcher template
    Added a basic GUIDE launcher template.

---
+++ GUIDE/GUIDE.py
@@ -0,0 +1,15 @@
+# Title : GUIDE Launcher
+# Project : GUI Designer DearPyGUI
+# Author : Tej Pandit
+# Date : 1 April 2024
+
+import dearpygui.dearpygui as dpg
+
+# DPG Context
+dpg.create_context()
+
+# DPG Viewport
+dpg.create_viewport(title="GUIDE : GUI Design Editor", width=1200, height=400)
+
+#dpg.start_dearpygui()
+dpg.destroy_context()
