d76d061a23d45cada1f437e4806952a1b20ccd97
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-04-02 20:23:16 -0500

    Added Component and Attribute Dictionary Variables
    Added class variables for component dictionaries and attribute dictionaries.

--- GUIDE/Components.py
+++ GUIDE/Components.py
@@ -7,11 +7,16 @@ import dearpygui.dearpygui as dpg
 
 class Components:
     def __init__(self):
-        self.components = {}
-        self.initComponents()
+        self.components = None
+        self.containers = None
+        self.attr_func = None
+        self.attr_types = None
+        self.attr_defaults = None
+        self.attr_mandatory = None
+        self.attr_permenant = None
+        self.Components()
+        self.Components()
+        self.Attributes()
 
-    def initComponents(self):
-        print("meow")
-        pass
 
 c = Components()
