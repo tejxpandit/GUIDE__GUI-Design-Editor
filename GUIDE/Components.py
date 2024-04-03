8c492d433a574fd34a98773daee901c185a6e411
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-04-02 20:26:05 -0500

    Added Button and Checkbox Components
    Added dictionary entries for the button and checkbox components along with their attributes.

--- GUIDE/Components.py
+++ GUIDE/Components.py
@@ -31,4 +31,46 @@ class Components:
             #         ....
             #     }
             # }
-        pass
+        self.components = {
+            # Button
+            "button" : {
+                "name"          : "Button",
+                "data_type"     : "trigger",
+                "function"      : "add_button",
+                "children"      : None,
+                "attributes"    : {
+                    "label"         : ["Label", "identifier", "Button"],
+                    "tag"           : ["Tag", "identifier", None],
+                    "parent"        : ["Parent", "identifier", None],
+                    "callback"      : ["Function", "function", None],
+
+                    "user_data"     : ["User Data", "any", None],
+                    "width"         : ["Width", "int", None],
+                    "height"        : ["Height", "int", None],
+                    "pos"           : ["Position", "tuple2", None],
+                    "show"          : ["Visible", "bool", True],
+                    "enabled"       : ["Enabled", "bool", True]
+                }
+            },
+            # CheckBox
+            "checkbox" : {
+                "name"          : "CheckBox",
+                "data_type"     : "bool",
+                "function"      : "add_checkbox",
+                "children"      : None,
+                "attributes"    : {
+                    "label"         : ["Label", "identifier", "CheckBox"],
+                    "tag"           : ["Tag", "identifier", None],
+                    "parent"        : ["Parent", "identifier", None],
+                    "callback"      : ["Function", "function", None],
+                    "default_value" : ["Default Value", "bool", False],
+                    
+                    "user_data"     : ["User Data", "any", None],
+                    "width"         : ["Width", "int", None],
+                    "height"        : ["Height", "int", None],
+                    "pos"           : ["Position", "tuple2", None],
+                    "show"          : ["Visible", "bool", True],
+                    "enabled"       : ["Enabled", "bool", True]
+                }
+            }
+        }
