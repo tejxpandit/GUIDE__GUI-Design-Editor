444942fc936d4d876b51b409743e9d1459cabcaf
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-04-04 17:52:13 -0500

    Add and Remove Component Tag Function
    Added functions for adding and removing tags when components and added and removed.

--- GUIDE/TagGenerator.py
+++ GUIDE/TagGenerator.py
@@ -14,3 +14,16 @@ class TagGenerator:
         tag = name + "_" + str(self.tag_id)
         return tag
     
+    def addComponent(self):
+        self.tag_id += 1
+        self.list_id.append(self.tag_id)
+
+    def removeComponent(self, name_id):
+        words = name_id.split("_")
+        id = words[1]
+        try:
+            self.list_id.remove(id)
+        except:
+            print("no such component ID : " + str(id))
+
+    # Clean-Up Unused IDs Later
