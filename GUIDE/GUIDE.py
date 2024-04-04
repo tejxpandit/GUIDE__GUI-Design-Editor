fba16d9b4f3e939a98c91af5f1286d4264e9d532
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-04-04 17:48:53 -0500

    Project and Data Management Objects
    Added project and data management objects. Also initialized the exit callback. Also added a special section for enabling debugging in DPG rendering system.

--- GUIDE/GUIDE.py
+++ GUIDE/GUIDE.py
@@ -4,12 +4,37 @@
 # Date : 1 April 2024
 
 import dearpygui.dearpygui as dpg
+from ProjectManager import ProjectManager
+from DataManager import DataManager
 
 # DPG Context
 dpg.create_context()
 
 # DPG Viewport
 dpg.create_viewport(title="GUIDE : GUI Design Editor", width=1200, height=400)
+
+# Data Manager
+dataman = DataManager()
+
+# DPG Window
+projectman = ProjectManager(dataman)
+projectman.projectManagementWindow()
+dpg.set_exit_callback(projectman.saveProjectOnExit)
+
+# DPG Render Context
+dpg.setup_dearpygui()
+dpg.show_viewport()
+
+# after "dpg.show_viewport()"
+###### For DEBUGGING ######
+dpg.configure_app(manual_callback_management=True)
+while dpg.is_dearpygui_running():
+    jobs = dpg.get_callback_queue()
+    dpg.run_callbacks(jobs)
+    dpg.render_dearpygui_frame()
+##########################
+# before "dpg.destroy_context()"
+# comment out "dpg.start_dearpygui()"
 
 #dpg.start_dearpygui()
 dpg.destroy_context()
