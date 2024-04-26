564cf2902fbdc8e38b0fb2830f4dbd8b66cf76ea
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-04-25 21:50:31 -0500

    GUIDE Editor Class
    Added a class for the editor of the GUIDE.

---
+++ GUIDE/Editor.py
@@ -0,0 +1,13 @@
+# Title : GUIDE : Editor
+# Project : GUI Designer DearPyGUI
+# Author : Tej Pandit
+# Date : 25 April 2024
+
+import copy as cpy
+import dearpygui.dearpygui as dpg
+from Components import Components
+from Builder import Builder
+
+class Editor:
+    def __init__(self, dataman):
+        self.dataman = dataman
